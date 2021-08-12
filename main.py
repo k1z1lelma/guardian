6dce3982f02c5f1383d73c29da78788dc7c36567
k1z1lelma <70051313+k1z1lelma@users.noreply.github.com>
2021-08-12 23:16:01 +0300

    version 0.1.1

--- main.py
+++ main.py
@@ -43,9 +43,10 @@ def main_page():
 def main_choose(main_choose):
     if (main_choose == "1"):
         banner()
-        print("The development process continues.")
-        sleep(1)
-        main_page()
+
+        security_menu()
+
+        security_process()
 
     elif (main_choose == "2"):
 
@@ -59,14 +60,17 @@ def main_choose(main_choose):
 
     elif (main_choose == "3"):
         banner()
-        print("The development process continues.")
-        sleep(1)
-        main_page()
+
+        optimization_menu()
+
+        optimization_process()
 
     else:
         print("Please enter a number between 1 and 4 !")
         sleep(2)
         main_page()
+
+# **************************
 
 def privacy_menu():
     print("\x1b[0;32m PRIVACY MENU \x1b[0m")
@@ -129,6 +133,183 @@ def privacy_process():
 
 def privacy_host_file_process():
     print("")
+
+# **************************
+
+def security_menu():
+    print("\x1b[0;32m SECURITY MENU \x1b[0m")
+    print("\n\x1b[0;32m [1]\x1b[0m Turn off USB storage")
+    print("\x1b[0;32m [2]\x1b[0m Turn off sharing windows folder")
+    print("\x1b[0;32m [3]\x1b[0m IP blocking")
+    print("\x1b[0;32m [4]\x1b[0m Activate HSTS (Strict Transport Security)")
+    print("\x1b[0;32m [5]\x1b[0m Turn off ping (by other computers)")
+    print("\x1b[0;32m [6]\x1b[0m Password protection")
+    print("\x1b[0;32m [7]\x1b[0m Encrypt patch file")
+    print("\x1b[0;32m [8]\x1b[0m Delete patch file (when computer shuts down)")
+    print("\x1b[0;32m [9]\x1b[0m Turn off remote registry access")
+    print("\x1b[0;32m [10]\x1b[0m Back\n")
+
+
+def security_process():
+    control = True
+    while (control == True):
+        security_choose = input("Your choice (1 - 10) :")
+        if (security_choose == "1" or security_choose == "2" or security_choose == "3" or security_choose == "4" or security_choose == "5" or security_choose == "6" or security_choose == "7" or security_choose == "8" or security_choose == "9" or security_choose == "10"):
+            control = False
+    system('cls')
+    if (security_choose == "1"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        security_menu()
+
+        security_process()
+    elif (security_choose == "2"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        security_menu()
+
+        security_process()
+    elif (security_choose == "3"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        security_menu()
+
+        security_process()
+    elif (security_choose == "4"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        security_menu()
+
+        security_process()
+    elif (security_choose == "5"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        security_menu()
+
+        security_process()
+    elif (security_choose == "6"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        security_menu()
+
+        security_process()
+    elif (security_choose == "7"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        security_menu()
+
+        security_process()
+    elif (security_choose == "8"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        security_menu()
+
+        security_process()
+    elif (security_choose == "9"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        security_menu()
+
+        security_process()
+    else:
+        main_page()
+
+# **************************
+
+def optimization_menu():
+    print("\x1b[0;32m OPTIMIZATION MENU \x1b[0m")
+    print("\n\x1b[0;32m [1]\x1b[0m Regular writing of Windows system files")
+    print("\x1b[0;32m [2]\x1b[0m Turn off unnecessary animations")
+    print("\x1b[0;32m [3]\x1b[0m Defrag the disk")
+    print("\x1b[0;32m [4]\x1b[0m Fix windows errors")
+    print("\x1b[0;32m [5]\x1b[0m Back\n")
+
+
+def optimization_process():
+    control = True
+    while (control == True):
+        optimization_choose = input("Your choice (1 - 5) :")
+        if (optimization_choose == "1" or optimization_choose == "2" or optimization_choose == "3" or optimization_choose == "4" or optimization_choose == "5"):
+            control = False
+    system('cls')
+    if (optimization_choose == "1"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        optimization_menu()
+
+        optimization_process()
+    elif (optimization_choose == "2"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        optimization_menu()
+
+        optimization_process()
+    elif (optimization_choose == "3"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        optimization_menu()
+
+        optimization_process()
+    elif (optimization_choose == "4"):
+        banner()
+        print("The development process continues.")
+        sleep(1)
+        system('cls')
+        banner()
+
+        optimization_menu()
+
+        optimization_process()
+    else:
+        main_page()
 
 main_page()
 
